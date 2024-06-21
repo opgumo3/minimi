24605c1d4cbcd8f0eddb7a11b85866055b73f675
opgumo <opgumo@naver.com>
2024-06-21 09:53:34 +0900

    reservation-back init

---
+++ reservation/reservation-back/src/main/java/com/reservation/ReservationBackApplication.java
@@ -0,0 +1,13 @@
+package com.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationBackApplication.class, args);
+	}
+
+}
