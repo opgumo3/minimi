94dbd477de328311a3a91dce45411f6e9ef29e59
opgumo <opgumo@naver.com>
2024-07-12 16:54:48 +0900

    dependency/domain 추가

---
+++ java/black_jack/src/main/java/org/minimi/domain/Card.java
@@ -0,0 +1,10 @@
+package org.minimi.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Card {
+	private String color;
+	private String value;
+	private String pattern;
+}
---
+++ java/black_jack/src/main/java/org/minimi/domain/Deck.java
@@ -0,0 +1,10 @@
+package org.minimi.domain;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class Deck {
+	private List<Card> cardList;
+}
